06dde7601cfafd83bff3c6c2006ade7052af5755
wlcy.leo <wlcy.leo@gmail.com>
2018-09-10 13:33:10 +0800

    update account synchronize logic

--- explorer/main/account/main.go
+++ explorer/main/account/main.go
@@ -9,7 +9,8 @@ import (
 var gIntMaxWorker = flag.Int("worker", 10, "maximum worker for fetch blocks")
 var gStrMysqlDSN = flag.String("dsn", "tron:tron@tcp(172.16.21.224:3306)/tron", "mysql connection string(DSN)")
 var gInt64MaxWorkload = flag.Int64("workload", 10000, "maximum workload for each worker")
-var gMaxBlockID = flag.Int64("max_block_id", 0, "max block")
+var gMaxBlockID = flag.Int64("max_block_id", 0, "max block num, 0 for current block, -1 will continue latest and do analyze until kill")
+var gMinBlockID = flag.Int64("start_block", 2200000, "block num start to analyze")
 
 func main() {
 	flag.Parse()
@@ -21,7 +22,24 @@ func main() {
 
 	initWorkerChan()
 
-	analyzeTrx(0, *gMaxBlockID)
+	if -1 == *gMaxBlockID {
+		b := *gMinBlockID
+		e := getDBMaxBlockID()
+		for {
+			fmt.Printf("Start account analyze for block range [%v] ~ [%v]\n", b, e)
+			ts := time.Now()
+			analyzeTrx(b, e)
+			tsCost := time.Since(ts)
+			if tsCost < time.Second*10 {
+				time.Sleep(10*time.Second - tsCost)
+			}
+			b = e
+			e = getDBMaxBlockID()
+		}
+	} else {
+		fmt.Printf("Start account analyze for block range [%v] ~ [%v]\n", *gMinBlockID, *gMaxBlockID)
+		analyzeTrx(*gMinBlockID, *gMaxBlockID)
+	}
 
 }
 
@@ -30,7 +48,7 @@ var trxBulkBlockNum = int64(10000)
 func analyzeTrx(b, e int64) {
 	startWorker()
 
-	if e == 0 {
+	if e <= 0 {
 		e = getDBMaxBlockID()
 	}
 
@@ -39,11 +57,16 @@ func analyzeTrx(b, e int64) {
 
 		b = e - trxBulkBlockNum
 	}
-
 	ts := time.Now()
+
 	blockIDs := genVerifyBlockIDList(b, e) // 7408
 	trxList := loadTransFromDB(blockIDs)
-	fmt.Printf("block range:[%v] ~ [%v], load %v trans cost:%v\n", b, e, len(trxList), time.Since(ts))
+	fmt.Printf("block range:[%v] ~ [%v] (%v blocks), load %v trans cost:%v\n", b, e, e-b, len(trxList), time.Since(ts))
+
+	if len(trxList) == 0 {
+		stopWorker()
+		return
+	}
 
 	for _, trx := range trxList {
 		trx.ExtractContract()
@@ -51,13 +74,13 @@ func analyzeTrx(b, e int64) {
 	}
 	updateTrxOwner(trxList)
 
-	waitCnt := 10
+	waitCnt := 3
 	for {
 		workerCnt := workingTaskCnt()
 		fmt.Printf("main routine, working task:%v, waitCnt:%v\n", workerCnt, waitCnt)
 		if workerCnt == 1 {
 			waitCnt--
-			if waitCnt < 0 {
+			if waitCnt <= 0 {
 				break
 			}
 
@@ -68,14 +91,14 @@ func analyzeTrx(b, e int64) {
 	stopWorker()
 
 	list, err := ClearRefreshAddress() // load all address from redis and prepare handle it
-	fmt.Println(err)
-	fmt.Printf("total account:%v\n", len(list))
+	fmt.Printf("total account:%v, err:%v\n", len(list), err)
 
 	accList, restAddr, _ := getAccount(list)
-	fmt.Printf("total account:%v, rest address:%v\n", len(accList), len(restAddr))
+	fmt.Printf("total account:%v, rest address:%v, cost:%v, synchronize to db .....\n", len(accList), len(restAddr), time.Since(ts))
+	ts = time.Now()
 	storeAccount(accList)
+	fmt.Printf("accList size:%v, restAddr size:%v, synchronze to DB cost:%v\n", len(accList), len(restAddr), time.Since(ts))
 
-	fmt.Printf("accList size:%v, restAddr size:%v\n", len(accList), len(restAddr))
 }
 
 func analyzeTrxFrk(b, e int64) {
@@ -89,7 +112,12 @@ func analyzeTrxFrk(b, e int64) {
 	ts := time.Now()
 	blockIDs := genVerifyBlockIDList(b, e)
 	trxList := loadTransFromDB(blockIDs)
-	fmt.Printf("block range:[%v] ~ [%v], load %v trans cost:%v\n", b, e, len(trxList), time.Since(ts))
+	fmt.Printf("block range:[%v] ~ [%v] (%v blocks), load %v trans cost:%v\n", b, e, e-b, len(trxList), time.Since(ts))
+
+	if len(trxList) == 0 {
+		stopWorker()
+		return
+	}
 
 	for _, trx := range trxList {
 		trx.ExtractContract()
