940243ae3b322fbb5be990d3478acd8ba6c5bb43
wlcy.leo <wlcy.leo@gmail.com>
2018-09-17 22:30:54 +0800

    fix tranx buffer

--- explorer/web/buffer/buffer_test/main.go
+++ explorer/web/buffer/buffer_test/main.go
@@ -26,9 +26,9 @@ func main() {
 		// getBlocks(-1, 500, 100) // get
 
 		// fmt.Println(bb.GetTransactionByBlockID(2414358))
-		r := bb.GetTransactionByBlockID(2414268)
+		r := bb.GetTransactionByBlockID(2288184)
 		for idx, trx := range r {
-			fmt.Printf("blockID:%v, %v-->%v\n", 2414268, idx, utils.ToJSONStr(trx))
+			fmt.Printf("blockID:%v, %v-->%v\n", 2288184, idx, utils.ToJSONStr(trx))
 		}
 		// getTrx()
 		// getTrxs()
