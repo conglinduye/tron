bf43b2fd5e8dc335a8ac335c133a9aa060b70ad3
yanzheng <yanzheng@tron.network>
2018-09-21 12:16:54 +0800

    接口

--- explorer/web/service/block_service.go
+++ explorer/web/service/block_service.go
@@ -110,18 +110,14 @@ func QueryBlockBuffer(req *entity.Blocks) (*entity.BlockInfo, error) {
 	block := &entity.BlockInfo{}
 	blockBuffer := buffer.GetBlockBuffer()
 	if req.Number != "" {
-		block = blockBuffer.GetBlock(mysql.ConvertStringToInt64(req.Number, 0))
-	}
-	return block, nil
-}
-
-//QueryBlockLatestBuffer 获取最新块
-func QueryBlockLatestBuffer() (*entity.BlockInfo, error) {
-	block := &entity.BlockInfo{}
-	blockBuffer := buffer.GetBlockBuffer()
-	maxNumber := blockBuffer.GetMaxBlockID()
-	if maxNumber > 0 {
-		block = blockBuffer.GetBlock(maxNumber)
+		if req.Number == "latest" {
+			maxNumber := blockBuffer.GetMaxBlockID()
+			if maxNumber > 0 {
+				block = blockBuffer.GetBlock(maxNumber)
+			}
+		} else {
+			block = blockBuffer.GetBlock(mysql.ConvertStringToInt64(req.Number, 0))
+		}
 	}
 	return block, nil
 }
