bf3c76ae39dc9ed74031dcc172b14efb38161781
yanzheng <yanzheng@tron.network>
2018-09-21 18:50:11 +0800

    fd

--- explorer/web/module/vote_module.go
+++ explorer/web/module/vote_module.go
@@ -238,3 +238,74 @@ func QueryVoteWitnessRanking(strSQL string) ([]*entity.VoteWitnessRanking, error
 
 	return voteWitnessRankingList, nil
 }
+
+//QueryVoteLiveRealize 操作数据库
+func QueryVoteLiveRealize(strSQL string) (*entity.VoteLiveInfo, error) {
+	log.Sql(strSQL)
+	dataPtr, err := mysql.QueryTableData(strSQL)
+	if err != nil {
+		log.Errorf("QueryVoteLiveRealize error :[%v]\n", err)
+		return nil, util.NewErrorMsg(util.Error_common_internal_error)
+	}
+	if dataPtr == nil {
+		log.Errorf("QueryVoteLiveRealize dataPtr is nil ")
+		return nil, util.NewErrorMsg(util.Error_common_internal_error)
+	}
+	votesResp := &entity.VoteLiveInfo{}
+	votesMap := make(map[string]*entity.LiveInfo, 0)
+
+	//填充数据
+	for dataPtr.NextT() {
+		var vote = &entity.LiveInfo{}
+		vote.Address = dataPtr.GetField("voteraddress")
+		vote.Name = dataPtr.GetField("account_name")
+		vote.Votes = mysql.ConvertDBValueToInt64(dataPtr.GetField("votes"))
+		vote.URL = dataPtr.GetField("url")
+		votesMap[vote.Address] = vote
+	}
+	votesResp.Data = votesMap
+
+	return votesResp, nil
+
+}
+
+//QueryVoteCurrentCycleRealize 操作数据库
+func QueryVoteCurrentCycleRealize(strSQL, filterSQL, sortSQL, pageSQL string) (*entity.VoteCurrentCycleResp, error) {
+	strFullSQL := strSQL + " " + filterSQL + " " + sortSQL + " " + pageSQL
+	log.Sql(strFullSQL)
+	dataPtr, err := mysql.QueryTableData(strFullSQL)
+	if err != nil {
+		log.Errorf("QueryVoteCurrentCycleRealize error :[%v]\n", err)
+		return nil, util.NewErrorMsg(util.Error_common_internal_error)
+	}
+	if dataPtr == nil {
+		log.Errorf("QueryVoteCurrentCycleRealize dataPtr is nil ")
+		return nil, util.NewErrorMsg(util.Error_common_internal_error)
+	}
+	votesResp := &entity.VoteCurrentCycleResp{}
+	voteCycle := make([]*entity.VoteCurrentCycle, 0)
+	var totalVotes = int64(0)
+
+	//填充数据
+	for dataPtr.NextT() {
+		var vote = &entity.VoteCurrentCycle{}
+		githubLink := dataPtr.GetField("github_link")
+		if githubLink != "" {
+			vote.HasPage = true
+		}
+
+		vote.Address = dataPtr.GetField("voteraddress")
+		vote.Name = dataPtr.GetField("account_name")
+		vote.Votes = mysql.ConvertDBValueToInt64(dataPtr.GetField("votes"))
+		vote.URL = dataPtr.GetField("url")
+
+		voteCycle = append(voteCycle, vote)
+	}
+	totalVotes = QueryTotalVotes()
+
+	votesResp.TotalVotes = totalVotes
+	votesResp.Candidates = voteCycle
+
+	return votesResp, nil
+
+}
