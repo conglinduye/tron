9f17d06b23baf0da6abf1215346ba0785150aba3
yanzheng <yanzheng@tron.network>
2018-09-23 18:40:19 +0800

    fd

--- explorer/lib/config/config_loader.go
+++ explorer/lib/config/config_loader.go
@@ -75,7 +75,7 @@ func initRedis(config *toml.Tree) error {
 		PoolSize int
 	}{}
 	redisInfo.Addr = config.GetDefault("Redis.host", "127.0.0.1:6379").(string)
-	redisInfo.Password = config.GetDefault("Redis.pass", "127.0.0.1:6379").(string)
+	redisInfo.Password = config.GetDefault("Redis.pass", "").(string)
 	redisInfo.Db = int(util.ToInt64(config.GetDefault("Redis.index", 0)))
 	redisInfo.PoolSize = int(util.ToInt64(config.GetDefault("Redis.poolSize", 10)))
 
