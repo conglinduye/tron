3da924f5a36251c34843b8f2483a23581c2e2f55
yanzheng <yanzheng@tron.network>
2018-09-25 17:05:18 +0800

    修改设置

--- explorer/lib/mysql/common_test.go
+++ explorer/lib/mysql/common_test.go
@@ -1,10 +1,24 @@
 package mysql
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestDistinct(t *testing.T) {
 	var data = []string{"a", "ab", "ac", "a1", "a2", "a", "a"}
 	ret, distinct := Distinct(data)
 	t.Log(ret, "\n")
 	t.Log(distinct, "\n")
+}
+
+func TestRemoveAddr(t *testing.T) {
+	ss := []string{"11", "bb", "fcc"}
+	for key, val := range ss {
+		if val == "fcc" {
+			kk := key + 1
+			ss = append(ss[:key], ss[kk:]...)
+		}
+	}
+	fmt.Printf("ss:[%v]", ss)
 }
