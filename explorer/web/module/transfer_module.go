5a4f0f3d7d3500228cad3d17f5fe5a76236605d5
yanzheng <yanzheng@tron.network>
2018-09-26 17:40:41 +0800

    按地址查询交易优化

--- explorer/web/module/transfer_module.go
+++ explorer/web/module/transfer_module.go
@@ -9,6 +9,63 @@ import (
 	"github.com/wlcy/tron/explorer/web/entity"
 )
 
+//QueryTransfersByAddressRealize 操作数据库
+func QueryTransfersByAddressRealize(strSQL string, needTotal bool) (*entity.TransfersResp, error) {
+	log.Sql(strSQL)
+	dataPtr, err := mysql.QueryTableData(strSQL)
+	if err != nil {
+		log.Errorf("QueryTransfersByAddressRealize error :[%v]\n", err)
+		return nil, util.NewErrorMsg(util.Error_common_internal_error)
+	}
+	if dataPtr == nil {
+		log.Errorf("QueryTransfersByAddressRealize dataPtr is nil ")
+		return nil, util.NewErrorMsg(util.Error_common_internal_error)
+	}
+	transfersResp := &entity.TransfersResp{}
+	transferInfos := make([]*entity.TransferInfo, 0)
+
+	//填充数据
+	for dataPtr.NextT() {
+		var transfer = &entity.TransferInfo{}
+		transfer.Block = mysql.ConvertDBValueToInt64(dataPtr.GetField("block_id"))
+		transfer.TransactionHash = dataPtr.GetField("trx_hash")
+		transfer.TransferFromAddress = dataPtr.GetField("owner_address")
+		createTime := dataPtr.GetField("create_time")
+		if len(createTime) > 13 {
+			createTime = createTime[:13]
+		}
+		transfer.CreateTime = mysql.ConvertDBValueToInt64(createTime)
+		transfer.TransferToAddress = dataPtr.GetField("to_address")
+		transfer.TokenName = dataPtr.GetField("asset_name")
+		transfer.Amount = mysql.ConvertDBValueToInt64(dataPtr.GetField("amount"))
+		if transfer.TokenName == "" {
+			transfer.TokenName = "TRX"
+			//如果是TRX，页面做的单位转换
+			//transfer.Amount = transfer.Amount / 1000000
+		}
+		confirmed := dataPtr.GetField("confirmed")
+		if confirmed == "1" {
+			transfer.Confirmed = true
+		}
+
+		transferInfos = append(transferInfos, transfer)
+	}
+
+	var total = int64(len(transferInfos))
+	if needTotal {
+		//查询该语句所查到的数据集合
+		total, err = mysql.QuerySQLViewCount(strSQL) //
+		if err != nil {
+			log.Errorf("query view count error:[%v], SQL:[%v]", err, strSQL)
+		}
+	}
+	transfersResp.Total = total
+	transfersResp.Data = transferInfos
+
+	return transfersResp, nil
+
+}
+
 //QueryTransfersRealize 操作数据库
 func QueryTransfersRealize(strSQL, filterSQL, sortSQL, pageSQL, filterTempSQL string, needTotal bool) (*entity.TransfersResp, error) {
 	strFullSQL := strSQL + " " + filterSQL + " " + sortSQL + " " + pageSQL
