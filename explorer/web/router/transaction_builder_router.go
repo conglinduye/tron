9b0c398fca476b95dc8f610c7dd5ebe25a95f0aa
wlcy.leo <wlcy.leo@gmail.com>
2018-10-08 20:47:56 +0800

    build transaction service imp...

--- explorer/web/router/transaction_builder_router.go
+++ explorer/web/router/transaction_builder_router.go
@@ -7,12 +7,6 @@ import (
 
 func registerTransactionBuilderRouter(router *gin.Engine) {
 
-	router.GET("/api/transaction-builder/contract/transfer", service.TBTransfer)
-	router.GET("/api/transaction-builder/contract/transferasset", service.TBTransferAsset)
-	router.GET("/api/transaction-builder/contract/accountcreate", service.TBAccountCreate)
-	router.GET("/api/transaction-builder/contract/accountupdate", service.TBAccountUpdate)
-	router.GET("/api/transaction-builder/contract/withdrawbalance", service.TBWithdrawBalance)
-
 	router.POST("/api/transaction-builder/contract/transfer", service.TBTransfer)
 	router.POST("/api/transaction-builder/contract/transferasset", service.TBTransferAsset)
 	router.POST("/api/transaction-builder/contract/accountcreate", service.TBAccountCreate)
